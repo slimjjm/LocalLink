ce10e8676406b82ed72efab86a61695965419d0a
James Evans <james22040@gmail.com>
2026-03-06 00:43:42 +0000

    LocalLink V2 checkpoint — booking engine stable

---
+++ LocalLink/Services/NextAvailabilitySlotService.swift
@@ -0,0 +1,7 @@
+//
+//  NextAvailabilitySe.swift
+//  LocalLink
+//
+//  Created by The Mac Daddy on 05/03/2026.
+//
+
---
+++ LocalLink/Views/BusinessQuickBookingView.swift
@@ -0,0 +1,7 @@
+//
+//  BusinessQuick.swift
+//  LocalLink
+//
+//  Created by The Mac Daddy on 05/03/2026.
+//
+
